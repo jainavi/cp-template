8763b7c088cc4c7ac82a6581d9b8fea1dd05f793
Avi Jain <jainavi1408@gmail.com>
2023-08-10 09:32:19 +0530

    Added a starter template

---
+++ starter-template.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fast_io              \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define ln "\n"
+#define ll long long
+#define vll vector<long long>
+#define vvll vector<vector<long long>>
+#define vint vector<int>
+#define vvint vector<vector<int>>
+#define v(x) vector<x>
+#define vv(x) vector<vector<x>>
+#define pii pair<int, int>
+#define pll pair<long, long>
+#define pb push_back
+#define popb pop_back
+#define rep(i, s, n) for (int i = s; i < n; i++)
+
+void solve()
+{
+}
+
+int main()
+{
+    fast_io;
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
